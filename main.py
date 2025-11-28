e75cff7e385f06873d4ac04ac90e197c5b96199e
MathiasDPX <mathias@dupeux.net>
2025-11-28 18:14:25 +0100

    move css to scss

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import sass
 
 app = Flask(__name__)
+
+
+@app.route("/css/<path:name>.css")
+def serve_scss(name):
+    scss_path = f"static/scss/{name}.scss"
+    css = sass.compile(filename=scss_path)
+    return Response(css, mimetype="text/css")
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)

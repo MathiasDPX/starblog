9b6c486723bcda8c34849329cebe8f1a05152a8c
MathiasDPX <mathias@dupeux.net>
2025-11-28 22:44:19 +0100

    add posts

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, render_template_string, abort
+from glob import glob
 import sass
 
 app = Flask(__name__)
+
+posts = {}
+for post in glob("posts/*.html"):
+    content = open(post, "r", encoding="utf-8").read()
+    title = post.replace("posts\\", "")
+    title = title.replace(".html", "")
+
+    print(f"Found post {title}")
+    posts[title] = content
 
 
 @app.route("/css/<path:name>.css")
@@ -9,6 +19,16 @@ def serve_scss(name):
     scss_path = f"static/scss/{name}.scss"
     css = sass.compile(filename=scss_path)
     return Response(css, mimetype="text/css")
+
+
+@app.route("/p/<string:name>")
+def serve_post(name):
+    content = posts.get(name)
+    
+    if content == None:
+        return abort(404)
+    
+    return render_template_string(content)
 
 
 @app.route("/")

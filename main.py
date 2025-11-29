3e4cf0e00a107d465340eb20da3cfa54717289c9
MathiasDPX <mathias@dupeux.net>
2025-11-29 02:56:58 +0100

    add rss/atom feeds

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 from flask import Flask, render_template, Response, render_template_string, abort
-from glob import glob
+from feedgen.feed import FeedGenerator
+from urllib.parse import urljoin
+import toml
 import sass
+import os
+
+with open('config.toml', 'r', encoding='utf-8') as f:
+    config = toml.loads(f.read())
 
 app = Flask(__name__)
+fg = FeedGenerator()
+
+fg.title(config['feed']['title'])
+fg.id(config['feed']['id'])
+fg.link(href=config['feed']['link'], rel='alternate')
+fg.description(config['feed']['description'])
+fg.logo(urljoin(config['feed']['link'], 'favicon.png'))
 
 posts = {}
-for post in glob("posts/*.html"):
-    content = open(post, "r", encoding="utf-8").read()
-    title = post.replace("posts\\", "")
-    title = title.replace(".html", "")
+for id, info in config["articles"].items():
+    fe = fg.add_entry()
+    fe.id(id)
+    fe.title(info['title'])
+    fe.description(info.get('description'))
 
-    print(f"Found post {title}")
+    content = open(os.path.join("posts", info['path']), "r", encoding="utf-8").read()
+    title = info['title']
+
     posts[title] = content
 
+
+@app.route("/atom.xml")
+def serve_atom():
+    data = fg.atom_str()
+    return Response(data, mimetype="application/atom+xml")
+
+@app.route("/rss.xml")
+def serve_rss():
+    data = fg.rss_str()
+    return Response(data, mimetype="application/rss+xml")
 
 @app.route("/css/<path:name>.css")
 def serve_scss(name):
